e4e069679f8f66d85ec2b68ab6e84293fe742829
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-04 21:27:45 +0100

    basic password manager class implementation

--- password_manager.py
+++ password_manager.py
@@ -8,3 +8,27 @@ class PasswordManager:
         self.key = self.generate_key(master_password)
         self.fernet = Fernet(self.key)
         self.passwords = {}
+        
+    def generate_key(self, master_password):
+        return Fernet.generate_key()
+    
+    def add_password(self, service, password):
+        encrypted_password = self.fernet.encrypt(password.encode())
+        self.passwords[service] = encrypted_password.decode()
+        
+    def get_password(self, service):
+        encrypted_password = self.passwords.get(service)
+        if encrypted_password:
+            return self.fernet.decrypt(encrypted_password.encode()).decode()
+        return None
+    
+    def list_services(self):
+        return list(self.passwords.keys())
+    
+    def save_to_file(self, filename):
+        with open(filename, 'w') as f:
+            json.dump(self.passwords, f)
+            
+    def load_from_file(self, filename):
+        with open(filename, 'r') as f:
+            self.passowrds = json.load(f)
