36efbe7f5df0a62ec9500820d93b107160f3caf7
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-22 23:20:25 +0100

    finished refactor

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from termcolor import colored
 GITHUB_REPO_URL = 'https://github.com/kiing-dom/cli-password-manager'
 PASSWORDS_FILE_PATH = 'passwords.json'
 
-def load_or_set_password(pm):
+def load_or_set_root_password(pm):
     """Loads or sets the root password based on whether the password file exists"""
     if os.path.exists(PASSWORDS_FILE_PATH):
         load_from_file(pm, PASSWORDS_FILE_PATH)
@@ -105,99 +105,33 @@ def main():
 
     pm = PasswordManager()
     
-    if os.path.exists('passwords.json'):
-        load_from_file(pm, 'passwords.json')
-        while True:
-            root_password = getpass.getpass(colored("Enter your root password: ", 'yellow'))
-            if pm.verify_root_password(root_password):
-                pm.set_root_password(root_password)
-                display_message("Access granted!", 'green')
-                break
-            else:
-                display_message("Root password Incorrect. Try again.", 'red')
-    else:
-        while True:
-            root_password = getpass.getpass(colored("Set a new root password: ", 'yellow'))
-            confirm_password = getpass.getpass(colored("Confirm root password: ", 'yellow'))
-            if root_password == confirm_password:
-                pm.set_root_password(root_password)
-                save_data_to_file(pm, 'passwords.json')
-                display_message("Root password set and saved successfully!", 'green')
-                break
-            else:
-                display_message("Passwords do not match. Try again.", 'red')
-                
+    load_or_set_root_password(pm)
+    
     while True:
         display_menu()
         choice = get_user_choice()
-        
+
         if choice == '1':
-            service = input(colored("Enter the service name: ", 'yellow'))
-            
-            if service in pm.list_services():
-                overwrite_choice = input(colored(f"A password for '{service}' exists already. Do you want to overwrite it? (Y/n): ", 'red'))
-                if overwrite_choice.lower() != 'y':
-                    display_message("Operation cancelled.", 'yellow')
-                    continue
-            password = getpass.getpass(colored("Enter the password: ", 'yellow'))
-            pm.add_password(service, password)
-            save_data_to_file(pm, 'passwords.json')
-            display_message("Password added successfully!", 'green')
-        
+            handle_add_password(pm)
         elif choice == '2':
-            service = input(colored("Enter the service name: ", 'yellow'))
-            password = pm.get_password(service)
-            if password:
-                display_message(f"Password for {service}: {password}", 'green')
-            else:
-                display_message("Service not found.", 'red')
-        
+            handle_get_password(pm)
         elif choice == '3':
             services = pm.list_services()
             display_message("Services:", 'cyan')
             for service in services:
                 display_message(f"- {service}", 'yellow')
-            
         elif choice == '4':
-            service = input(colored("Enter name of service with password to be edited: ", 'yellow'))
-            if service in pm.list_services():
-                new_password = getpass.getpass(colored("Enter the new password: ", 'yellow'))
-                if pm.edit_password(service, new_password):
-                    display_message(f"Password for {service} updated successfully!", 'green')
-                else:
-                    display_message(f"Password could not be updated!", 'red')
-            else:
-                display_message(f"Service could not be found", 'red')
-                
+            handle_edit_password(pm)
         elif choice == '5':
-            service = input(colored("Enter name of service to be deleted: ", 'yellow'))
-            if service in pm.list_services():
-                confirm = input(colored(f"Are you sure you want to delete the password for {service}? (y/n): ", 'yellow')).lower()
-                if confirm == 'y':
-                    if(pm.delete_password(service)):
-                        display_message(f"Password for {service} deleted successfully!", 'green')
-                        save_data_to_file(pm, 'passwords.json')
-                    else:
-                        display_message(f"Password for {service} could not be deleted", 'red')
-                else: display_message("Deletion cancelled.", 'yellow')
-            else: display_message(f"Service {service} does not exist.", 'red')
-
+            handle_delete_password(pm)
         elif choice == '6':
-            length = int(input(colored("Enter a password length: ", 'yellow')))
-            if length < 12:
-                display_message("Invalid length. Password length should be at least 12")
-            else:
-                password = pm.generate_strong_password(length)
-                display_message(f"Password: {password}")
-            
+            handle_generate_password(pm)
         elif choice == '7':
             display_message("Opening GitHub repo in browser...", 'cyan')
             webbrowser.open(GITHUB_REPO_URL)
-        
         elif choice == '8':
             display_message("Thank you for staying Auth the Grid. Goodbye!", 'red')
             sys.exit(0)
-        
         else:
             display_message("Invalid choice. Please try again.", 'red')
 
